629d2c3d55a143b0987514cd9a4b97531cb6f888
Yves Van Broekhoven <yvesvanbroekhoven@gmail.com>
2025-07-01 09:26:48 +0200

    feat(app): started main template and homepage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Cutive+Mono&display=swap'
+        }
+      ]
+    }
+  },
   compatibilityDate: '2025-05-15',
+  css: ['~/assets/css/main.css'],
   devtools: { enabled: true }
 })
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>
+    <span>Willy </span>
+    <span>Van </span>
+    <span>Broekhoven</span>
+  </h1>
+
+  <h2>
+    <span>Natuur </span>
+    <span>Fotografie</span>
+  </h2>
+</template>
